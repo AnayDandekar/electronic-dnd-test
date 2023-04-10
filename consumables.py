989ee6047262bc6cbef52b880fb356ae75f985ac
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 20:09:27 -0700

    added consumables class

---
+++ consumables.py
@@ -0,0 +1,5 @@
+class Consumables:
+    def __init__(self, name, affected_stat, stat_change):
+        self.name = name
+        self.affected_stat = affected_stat
+        self.stat_change = stat_change
