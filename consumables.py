268a6c182bf4d768ac647dd40b31fc6808c457be
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 22:28:48 -0700

    added basic consumables

--- consumables.py
+++ consumables.py
@@ -1,5 +1,20 @@
-class Consumables:
+# consumables class
+class Consumable:
     def __init__(self, name, affected_stat, stat_change):
         self.name = name
         self.affected_stat = affected_stat
         self.stat_change = stat_change
+        
+# variables of consumable items
+bread = Consumable("bread", "hunger", 2)
+raw_meat = Consumable("raw meat", "hunger", 4)
+health_potion = Consumable("health potion", "health", 5)
+water_bottle = Consumable("water bottle", "thirst", 2)
+
+# dictionary of consumable items
+consumable_items = {
+    "bread": bread,
+    "raw meat": raw_meat,
+    "health potion": health_potion,
+    "water bottle": water_bottle
+}
