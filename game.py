d130af0094577595ac80516376a65760da4a13df
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 20:26:07 -0700

    moved player class to separate file

--- game.py
+++ game.py
@@ -1,10 +1,4 @@
 import os
 
-#player class
-class Player:
-    def __init__(self, player_num, inventory):
-        self.player_num = player_num
-        self.inventory = inventory
-
 # speak welcome text
-os.system("espeak \"welcome to electronic dungeons and dragons\"")
+os.system("espeak 'welcome to electronic dungeons and dragons'")
---
+++ players.py
@@ -0,0 +1,4 @@
+class Player:
+    def __init__(self, player_num, inventory):
+        self.player_num = player_num
+        self.inventory = inventory
