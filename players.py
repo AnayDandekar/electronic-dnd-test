0fd7ffdf913f7f5d09affa0f04f32d726cba06a7
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 22:44:43 -0700

    added more player stats

--- players.py
+++ players.py
@@ -1,4 +1,8 @@
+# players class
 class Player:
-    def __init__(self, player_num, inventory):
+    def __init__(self, player_num, inventory, health, hunger, thirst):
         self.player_num = player_num
         self.inventory = inventory
+        self.health = health
+        self.hunger = hunger
+        self.thirst = thirst
