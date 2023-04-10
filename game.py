7f1f6dc3bc6f03e93a15e4cce5a9249d9161fb0d
Anay Dandekar <anaydandekar@gmail.com>
2023-04-10 12:36:02 -0700

    added quickstart guide

--- game.py
+++ game.py
@@ -4,11 +4,17 @@ from consumables import consumable_items
 from weapons import weapons
 from players import Player
 
-# set players
+# variables of players
 player1 = Player(1, [], 15, 1, 2)
 player2 = Player(2, [], 18, 2, 5)
 player3 = Player(3, [], 13, 4, 1)
 player4 = Player(4, [], 16, 1, 3)
 
 # speak welcome text
-os.system("espeak -g 25 -s 125 'welcome to electronic dungeons and dragons'")
+os.system("espeak -g 25 -s 125 'welcome to electronic dungeons and dragons. would you like to listen to the quickstart guide?'")
+
+# yes or no input for quickstart guide
+answer = input("Quickstart Guide? ")
+
+if answer == "yes":
+    os.system("espeak -g 25 -s 125 -f quickstart.txt")
