3eddcc8d9a0347d78b00099aca07c3f2e3d98d65
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 22:53:32 -0700

    imported data from other files

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
+# import modules
 import os
+# import assets
+from consumables import consumable_items
+from weapons import weapons
+from players import Player
 
 # speak welcome text
-os.system("espeak 'welcome to electronic dungeons and dragons'")
+# os.system("espeak 'welcome to electronic dungeons and dragons'")
+print(consumable_items["raw meat"].stat_change)
