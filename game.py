61994ac753e0ca320b60b077fb0bd1c8c1dae9d2
Anay Dandekar <anaydandekar@gmail.com>
2023-04-10 12:42:01 -0700

    added position values to variables of players

--- game.py
+++ game.py
@@ -5,10 +5,10 @@ from weapons import weapons
 from players import Player
 
 # variables of players
-player1 = Player(1, [], 15, 1, 2)
-player2 = Player(2, [], 18, 2, 5)
-player3 = Player(3, [], 13, 4, 1)
-player4 = Player(4, [], 16, 1, 3)
+player1 = Player(1, [], 15, 1, 2, 10)
+player2 = Player(2, [], 18, 2, 5, 25)
+player3 = Player(3, [], 13, 4, 1, 8)
+player4 = Player(4, [], 16, 1, 3, 17)
 
 # speak welcome text
 os.system("espeak -g 25 -s 125 'welcome to electronic dungeons and dragons. would you like to listen to the quickstart guide?'")
