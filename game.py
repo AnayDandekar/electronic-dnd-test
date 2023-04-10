1f81f8969183a4135052d100f4d44eb229d55bc6
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 23:00:06 -0700

    added mock player data

--- game.py
+++ game.py
@@ -5,6 +5,11 @@ from consumables import consumable_items
 from weapons import weapons
 from players import Player
 
+# set players
+player1 = Player(1, [], 15, 1, 2)
+player2 = Player(2, [], 18, 2, 5)
+player3 = Player(3, [], 13, 4, 1)
+player4 = Player(4, [], 16, 1, 3)
+
 # speak welcome text
-# os.system("espeak 'welcome to electronic dungeons and dragons'")
-print(consumable_items["raw meat"].stat_change)
+os.system("espeak 'welcome to electronic dungeons and dragons'")
