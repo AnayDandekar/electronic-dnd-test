b3a984fcee27f0063a19375351f1cdb4e696dc65
Anay Dandekar <anaydandekar@gmail.com>
2023-04-10 12:21:06 -0700

    added position attribute to player class

--- players.py
+++ players.py
@@ -1,8 +1,9 @@
 # players class
 class Player:
-    def __init__(self, player_num, inventory, health, hunger, thirst):
+    def __init__(self, player_num, inventory, health, position, hunger, thirst):
         self.player_num = player_num
         self.inventory = inventory
         self.health = health
+        self.position = position
         self.hunger = hunger
         self.thirst = thirst
