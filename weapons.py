4c3ea09042b4d8656765355395c779a6bed5e5e9
Anay Dandekar <anaydandekar@gmail.com>
2023-04-09 22:38:25 -0700

    added basic weapons

--- weapons.py
+++ weapons.py
@@ -1,4 +1,20 @@
+# weapons class
 class Weapon:
-    def __init__(self, name, damage):
+    def __init__(self, name, damage, weapon_range):
         self.name = name
         self.damage = damage
+        self.weapon_type = weapon_type
+
+# variables of weapons
+sword = Weapon("sword", 5, "short")
+bow_and_arrow = Weapon("bow and arrow", 2, "long")
+hammer = Weapon("hammer", 7, "short")
+dagger = Weapon("dagger", 3, "short")
+
+# dictionary of weapons
+weapons = {
+    "sword": sword,
+    "bow and arrow": bow_and_arrow,
+    "hammer": hammer,
+    "dagger": dagger
+}
