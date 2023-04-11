7c7137c8ea2b7bf8a6d597a31c7cbadf7ef06b9b
Anay <anaydandekar@gmail.com>
2023-04-11 15:05:12 -0700

    added a check to make sure that an item being crafted exists in the game

--- players.py
+++ players.py
@@ -1,6 +1,5 @@
 # import assets
 from voicefunctions import speak_text, voice_speed, space_pause_len
-from materials import materials
 from items import items
 
 # players class
@@ -13,17 +12,20 @@ class Player:
         self.hunger = hunger
         self.thirst = thirst
     def craft_item(self, item):
-        for material in item.materials:
-            # check if all materials required to craft item are in player's inventory
-            if material not in self.inventory:
-                # notify player that they don't have all the required materials to craft the item
-                speak_text(f"you don't have the materials to craft {item}", False, voice_speed, space_pause_len)
-                break
-        else:
-            # if all items required for crafting are present, remove them from player's inventory
+        if item in items:
             for material in item.materials:
-                self.inventory.remove(material)
-            # add crafted item to player's inventory
-            self.inventory.append(item)
-            # notify player that new item has been crafted
-            speak_text(f"you successfully crafted {item}", False, voice_speed, space_pause_len)
+                # check if all materials required to craft item are in player's inventory
+                if material not in self.inventory:
+                    # notify player that they don't have all the required materials to craft the item
+                    speak_text(f"you don't have the materials to craft {item}", False, voice_speed, space_pause_len)
+                    break
+            else:
+                # if all items required for crafting are present, remove them from player's inventory
+                for material in item.materials:
+                    self.inventory.remove(material)
+                # add crafted item to player's inventory
+                self.inventory.append(item)
+                # notify player that new item has been crafted
+                speak_text(f"you successfully crafted {item}", False, voice_speed, space_pause_len)
+        else:
+            speak_text(f"{item} does not exist in the game")
