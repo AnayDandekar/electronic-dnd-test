56dc9db92a17bc22480e1954f7f919c52482bbd3
Anay <anaydandekar@gmail.com>
2023-04-11 13:08:02 -0700

    added clothing subclass under item class

--- items.py
+++ items.py
@@ -14,11 +14,23 @@ class Weapon(Item):
         self.damage = damage
         self.weapon_range = weapon_range
 
+# clothing subclass
+class Clothing(Item):
+    def __init__(self, name, materials, health_bonus):
+        super().__init__(name, materials)
+        self.health_bonus = health_bonus
+
 # variables of weapons
 sword = Weapon("sword", [materials.get("wood")], 5, "short")
 bow_and_arrow = Weapon("bow and arrow", [materials.get("wood")], 2, "long")
 hammer = Weapon("hammer", 7, [materials.get("wood")], "short")
 dagger = Weapon("dagger", 3, [materials.get("wood")], "short")
+
+# variables of clothing
+cloth_shirt = Clothing("cloth shirt", [materials.get("cloth"), materials.get("thread")], 1)
+cloth_pants = Clothing("cloth pants", [materials.get("cloth"), materials.get("thread")], 1)
+cloth_helmet = Clothing("cloth helmet", [materials.get("cloth"), materials.get("thread")], 1)
+cloth_boots = Clothing("cloth boots", [materials.get("cloth"), materials.get("thread")], 1)
 
 # dictionary of items
 items = {
@@ -27,5 +39,11 @@ items = {
         "bow and arrow": bow_and_arrow,
         "hammer": hammer,
         "dagger": dagger
-        }
+    },
+    "clothing": {
+        "cloth shirt": cloth_shirt,
+        "cloth pants": cloth_pants,
+        "cloth helmet": cloth_helmet,
+        "cloth boots": cloth_boots
+    }
 }
