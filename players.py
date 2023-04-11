722d14bc704916275f39cfdfef79e566987e22da
Anay <anaydandekar@gmail.com>
2023-04-11 14:31:06 -0700

    added functionality to empty craft item function

--- players.py
+++ players.py
@@ -1,4 +1,5 @@
 # import assets
+from voicefunctions import speak_text
 from materials import materials
 from items import items
 
@@ -11,5 +12,18 @@ class Player:
         self.position = position
         self.hunger = hunger
         self.thirst = thirst
-    def craft_item(self, materials, crafted_item):
-        pass
+    def craft_item(self, item):
+        for material in item.materials:
+            # check if all materials required to craft item are in player's inventory
+            if material not in self.inventory:
+                # notify player that they don't have all the required materials to craft the item
+                speak_text(f"you don't have the materials to craft {item}", False, 120, 10)
+                break
+        else:
+            # if all items required for crafting are present, remove them from player's inventory
+            for material in item.materials:
+                self.inventory.remove(material)
+            # add crafted item to player's inventory
+            self.inventory.append(item)
+            # notify player that new item has been crafted
+            speak_text(f"you successfully crafted {item}", False, 120, 10)
