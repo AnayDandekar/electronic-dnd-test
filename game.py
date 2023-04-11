e72f12442fdd1062b3e17ec4cbfcc9662748d0c7
Anay Dandekar <anaydandekar@gmail.com>
2023-04-10 19:24:54 -0700

    added text to speech function

--- game.py
+++ game.py
@@ -4,11 +4,23 @@ from consumables import consumable_items
 from weapons import weapons
 from players import Player
 
+# default text to speech settings
+voice_speed = 120
+space_pause_len = 10
+
 # set mock player data
 player1 = Player(1, [], 15, 1, 2, 10)
 player2 = Player(2, [], 18, 2, 5, 25)
 player3 = Player(3, [], 13, 4, 1, 8)
 player4 = Player(4, [], 16, 1, 3, 17)
+
+def speak_text(text, is_file, voice_speed, space_pause_len):
+    # speak text with settings from function parameters
+    if not is_file:
+        os.system(f"espeak -s {voice_speed} -g {space_pause_len} '{text}'")
+    # speak file with settings from function parameters
+    if is_file:
+        os.system(f"espeak -s {voice_speed} -g {space_pause_len} -f {text}")
 
 def ask_question(prompt):
     # use text input to receive information from user (temporary)
@@ -16,11 +28,10 @@ def ask_question(prompt):
     return response
 
 # speak welcome text
-os.system("espeak -g 25 -s 125 'welcome to electronic dungeons and dragons'")
+speak_text("welcome to electronic dungeons and dragons", False, voice_speed, space_pause_len)
 
-# yes or no input for quickstart guide
-os.system("espeak -g 25 -s 125 'would you like to listen to the quickstart guide?'")
-
-quickstart_guide_question = ask_question("would you like to listen to the quickstart guide?")
+# quickstart guide option
+speak_text("would you like to listen to the quickstart guide?", False, voice_speed, space_pause_len)
+quickstart_guide_question = ask_question("Listen to quickstart guide?")
 if quickstart_guide_question == "yes":
-    os.system("espeak -g 25 -s 125 -f quickstart.txt")
+    speak_text("quickstart.txt", True, voice_speed, space_pause_len)
